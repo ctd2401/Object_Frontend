ac0ab732633c78401b50bb4c3866c2edd1adb23f
Hien.Ngo <hienhhh1712@gmail.com>
2025-07-29 10:17:15 +0700

    prettier các trang

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import AppLayout from '@/layout/AppLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-
+import { uikitRouter } from './uikit.router';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -13,82 +13,7 @@ const router = createRouter({
           name: 'dashboard',
           component: () => import('@/views/Dashboard.vue')
         },
-        {
-          path: '/uikit/formlayout',
-          name: 'formlayout',
-          component: () => import('@/views/uikit/FormLayout.vue')
-        },
-        {
-          path: '/uikit/input',
-          name: 'input',
-          component: () => import('@/views/uikit/InputDoc.vue')
-        },
-        {
-          path: '/uikit/button',
-          name: 'button',
-          component: () => import('@/views/uikit/ButtonDoc.vue')
-        },
-        {
-          path: '/uikit/table',
-          name: 'table',
-          component: () => import('@/views/uikit/TableDoc.vue')
-        },
-        {
-          path: '/uikit/list',
-          name: 'list',
-          component: () => import('@/views/uikit/ListDoc.vue')
-        },
-        {
-          path: '/uikit/tree',
-          name: 'tree',
-          component: () => import('@/views/uikit/TreeDoc.vue')
-        },
-        {
-          path: '/uikit/panel',
-          name: 'panel',
-          component: () => import('@/views/uikit/PanelsDoc.vue')
-        },
-
-        {
-          path: '/uikit/overlay',
-          name: 'overlay',
-          component: () => import('@/views/uikit/OverlayDoc.vue')
-        },
-        {
-          path: '/uikit/media',
-          name: 'media',
-          component: () => import('@/views/uikit/MediaDoc.vue')
-        },
-        {
-          path: '/uikit/message',
-          name: 'message',
-          component: () => import('@/views/uikit/MessagesDoc.vue')
-        },
-        {
-          path: '/uikit/file',
-          name: 'file',
-          component: () => import('@/views/uikit/FileDoc.vue')
-        },
-        {
-          path: '/uikit/menu',
-          name: 'menu',
-          component: () => import('@/views/uikit/MenuDoc.vue')
-        },
-        {
-          path: '/uikit/charts',
-          name: 'charts',
-          component: () => import('@/views/uikit/ChartDoc.vue')
-        },
-        {
-          path: '/uikit/misc',
-          name: 'misc',
-          component: () => import('@/views/uikit/MiscDoc.vue')
-        },
-        {
-          path: '/uikit/timeline',
-          name: 'timeline',
-          component: () => import('@/views/uikit/TimelineDoc.vue')
-        },
+        ...uikitRouter,
         {
           path: '/pages/empty',
           name: 'empty',
@@ -103,7 +28,22 @@ const router = createRouter({
           path: '/pages/documentation',
           name: 'documentation',
           component: () => import('@/views/pages/Documentation.vue')
-        }
+        },
+        {
+          path: '/customer',
+          name: 'ListCustomer',
+          component: () => import('@/views/my-account/ListCustomer.vue')
+        },
+        {
+          path: '/store',
+          name: 'ListStore',
+          component: () => import('@/views/my-account/ListStore.vue')
+        },
+        {
+          path: '/banner',
+          name: 'Banner',
+          component: () => import('@/views/my-account/Banner.vue')
+        },
       ]
     },
     {
