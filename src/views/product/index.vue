95695da26fa1b757783b976168cb2bffbd688b59
Z <chiennotforwork@gmail.com>
2025-10-01 18:42:00 +0700

    update

--- src/views/product/index.vue
+++ src/views/product/index.vue
@@ -190,6 +190,7 @@ onUnmounted(() => {
 
       <!-- Grid sản phẩm -->
       <div
+        ref="productGrid"
         v-else
         class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4"
       >
